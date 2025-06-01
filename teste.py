f5e540ebad48c73bf39f58a32827e4c8fe2be7c8
Edu Freire <125585132+eduufreire@users.noreply.github.com>
2025-06-01 11:32:34 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,8 +1,20 @@
 import papermill as pm
+import concurrent.futures
 
-pm.execute_notebook(
-    '../../opt/jupyter/notebook/limpeza-camera-visao-comp.ipynb',
-    '../../opt/jupyter/notebook/limpeza-camera-visao-comp-exec.ipynb',
-    parameters=dict(nome_arquivo="visao_computacional.csv"),
-    execution_timeout=300
-)
+def executar_notebook():
+    pm.execute_notebook(
+        '../../opt/jupyter/notebook/limpeza-camera-visao-comp.ipynb',
+        '../../opt/jupyter/notebook/limpeza-camera-visao-comp-exec.ipynb',
+        parameters=dict(nome_arquivo="visao_computacional.csv")
+    )
+
+timeout_global = 300
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
+    futuro = executor.submit(executar_notebook)
+    try:
+        # Espera a execução terminar ou dá timeout
+        resultado = futuro.result(timeout=timeout_global)
+        print("Notebook executado com sucesso!")
+    except concurrent.futures.TimeoutError:
+        print(f"Timeout global de {timeout_global} segundos excedido! Abortando...")
