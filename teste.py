b13319750e320e8a1b00a9f5175088c97f56af66
Edu Freire <125585132+eduufreire@users.noreply.github.com>
2025-06-01 10:33:35 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,7 @@
+import papermill as pm
+
+pm.execute_notebook(
+    '/opt/jupyter/notebook/limpeza-camera-visao-comp.ipynb',
+    '/opt/jupyter/notebook/limpeza-camera-visao-comp-exec.ipynb',
+    parameters=dict(nome_arquivo="visao_computacional.csv")
+)
