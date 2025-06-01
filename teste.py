f56d3878375615c76e08533a8d41e8c58a490ef7
Edu Freire <125585132+eduufreire@users.noreply.github.com>
2025-06-01 10:46:12 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -3,5 +3,6 @@ import papermill as pm
 pm.execute_notebook(
     '../../opt/jupyter/notebook/limpeza-camera-visao-comp.ipynb',
     '../../opt/jupyter/notebook/limpeza-camera-visao-comp-exec.ipynb',
-    parameters=dict(nome_arquivo="visao_computacional.csv")
+    parameters=dict(nome_arquivo="visao_computacional.csv"),
+    execution_timeout=300
 )
